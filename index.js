1a0abdec64194fa696f73cec3cfa98a11f3c6e4e
Aysia Brown <aysiaebrown@gmail.com>
2020-12-07 18:45:12 -0800

    updated to use PORT variable

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict';
 
 require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 
 // to grab modules exported from server.js
 const server = require('./server.js');
 
-server.start(3000);
+server.start(PORT);
